c9deb5955df465c10ce327d83c094575d3f8c8f7
Jishnu P <jishnupunathil000@gmail.com>
2022-08-03 13:55:51 +0530

    register mail added

--- frontend/src/app/sign-up/sign-up.component.ts
+++ frontend/src/app/sign-up/sign-up.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -66,7 +67,16 @@ export class SignUpComponent implements OnInit {
 
         this.isProcessing = false
         this.toast.success('Registration  SuccessFull', 'Success')
-        this.router.navigate(['/login'])
+
+        this.auth.sendEmail('http://localhost:3000/sendmail',formData).subscribe(
+          data=>{
+            let res:any=data
+            console.log(`${formData.firstname} is successfully registered`);
+            
+          }
+        )
+
+        // this.router.navigate(['/login'])
       }
       else {
 
