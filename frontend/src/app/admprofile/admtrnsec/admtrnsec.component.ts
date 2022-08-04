183f4a236a774647c14a21adbb83f3b2954a7245
Thushara Chandran <thushchand5@gmail.com>
2022-08-04 16:25:35 +0530

    add , delete button in admin

--- frontend/src/app/admprofile/admtrnsec/admtrnsec.component.ts
+++ frontend/src/app/admprofile/admtrnsec/admtrnsec.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TrainerService } from 'src/app/service/trainer.service';
 import { Router } from '@angular/router';
-
+import { NgForm } from '@angular/forms';
+declare var M: any;
 @Component({
   selector: 'app-admtrnsec',
   templateUrl: './admtrnsec.component.html',
@@ -22,5 +23,14 @@ export class AdmtrnsecComponent implements OnInit {
 logout(){
   localStorage.clear()
   this.router.navigate(['/'])
+}
+
+onDelete(id:any) {
+  if (confirm('Are you sure to delete this record ?') == true) {
+    this.trainerservice.onDelete(id).subscribe((res) => {
+    
+      M.toast({ html: 'Deleted successfully', classes: 'rounded' });
+    });
+  }
 }
 }
